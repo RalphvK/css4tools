e318b0a67cb2d9101a84f86613f3e7546f31fc5e
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2019-01-12 13:21:38 +0100

    add color displays

--- js/scripts.js
+++ js/scripts.js
@@ -444,9 +444,11 @@ const hex_rgb = {
 const scss = {
     data: function () {
         return {
-            baseColor: '#000',
-            variantColor: '#000',
-            scss: 'test'
+            baseColor: '#00004d',
+            baseColorDisplay: '#00004d',
+            variantColor: '#3d24ff',
+            variantColorDisplay: '#3d24ff',
+            scss: '// SCSS goes here'
         }
     },
     watch: {
@@ -465,19 +467,21 @@ const scss = {
     },
     methods: {
         init() {
+            this.setBackground('body', '#e6e6e6');
             this.update();
         },
         update() {
             // create color objects
             var bc = new colorCode(this.baseColor);
             var vc = new colorCode(this.variantColor);
-            // set background color
-            this.setBackground('body', bc.hex(), vc.hex());
+            // set color displays
+            this.baseColorDisplay = bc.hex();
+            this.variantColorDisplay = vc.hex();
             // set new scss
             return this.scss = bc.scssTransformTo(vc);
         },
-        setBackground(selector, colorA, colorB) {
-            document.querySelector(selector).style.background = `linear-gradient(to right, ${colorA} 50%, ${colorB} 50%)`;
+        setBackground(selector, value) {
+            document.querySelector(selector).style.background = value;
         }
     },
     mounted() {
@@ -488,16 +492,24 @@ const scss = {
 
             <!-- input row -->
             <div id="row-input">
-                <form class="row">
+                <div id="row-color" class="row animated fadeInDown" style="animation-delay: 0.3s;">
                     <div class="col-md-6">
-                        <div class="form-group animated fadeInDown">
+                        <div class="color-display form-group" v-bind:style="{ backgroundColor: baseColorDisplay}"></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="color-display form-group" v-bind:style="{ backgroundColor: variantColorDisplay}"></div>
+                    </div>
+                </div>
+                <form class="row animated fadeInDown" style="animation-delay: 0.1s;">
+                    <div class="col-md-6">
+                        <div class="form-group">
                             <b-input-group prepend="FROM">
                                 <b-form-input v-model="baseColor" type="text" size="lg"></b-form-input>
                             </b-input-group>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="form-group animated fadeInDown" style="animation-delay: 0.1s;">
+                        <div class="form-group">
                             <b-input-group prepend="TO">
                                 <b-form-input v-model="variantColor" type="text" size="lg"></b-form-input>
                             </b-input-group>
@@ -506,7 +518,7 @@ const scss = {
                 </form>
                 <form class="row">
                     <div class="col">
-                        <div class="form-group animated fadeInDown code" style="animation-delay: 0.1s;">
+                        <div class="form-group animated fadeInDown code" style="animation-delay: 0s;">
                             <b-input-group>
                                 <b-form-textarea v-model="scss" type="text" size="lg" :rows="3" @focus.native="$event.target.select()" @click.native="$event.target.select()" readonly></b-form-textarea>
                             </b-input-group>
